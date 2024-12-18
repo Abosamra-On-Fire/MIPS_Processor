affde9a462dfc7e2d3b1dd6072cfcb3d11f9059d
nourabouzeid <noody081@gmail.com>
2024-12-18 21:28:01 +0200

    :sparkles: read instruction memory from file

--- IM.vhd
+++ IM.vhd
@@ -1,5 +1,7 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_TEXTIO.ALL;
+USE STD.TEXTIO.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY IM IS
@@ -14,28 +16,31 @@ ENTITY IM IS
 END IM;
 
 ARCHITECTURE Behavioral OF IM IS
-    TYPE memory_array IS ARRAY (0 TO 4) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
-    SIGNAL instruction_memory : memory_array := (
-        "0100001000000100",
-        "0001000000000000",
-        "0001100100100000",
-        "0011101101000000",
-        "0101010001000000"
-    );
+    TYPE memory_array IS ARRAY (0 TO 10) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
+    IMPURE FUNCTION init_ram_bin RETURN memory_array IS
+        FILE text_file : text OPEN read_mode IS "IM.txt";
+        VARIABLE text_line : line;
+        VARIABLE ram_content : memory_array;
+        VARIABLE bv : bit_vector(ram_content(0)'RANGE);
+        VARIABLE i : INTEGER := 0;
+    BEGIN
+        WHILE NOT endfile(text_file) LOOP
+            readline(text_file, text_line);
+            read(text_line, bv);
+            ram_content(i) := to_stdlogicvector(bv);
+            i := i + 1;
+        END LOOP;
+        RETURN ram_content;
+    END FUNCTION;
+    SIGNAL instruction_memory : memory_array := init_ram_bin;
 
 BEGIN
     PROCESS (clk, reset)
     BEGIN
         IF reset = '1' THEN
             instruction <= (OTHERS => '0');
-
         ELSE
             IF rising_edge(clk) THEN
-                -- if load = '1' then
-                -- for i in 0 to 1 loop
-                --     instruction_memory(i) <= program((i+1)*16-1 downto i*16);
-                -- end loop;
-                -- else
                 instruction <= instruction_memory(to_integer(unsigned(location)));
             END IF;
         END IF;
