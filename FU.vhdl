692d3fbe8b6062978f6d2e4d82b941885382608a
Mariam Amin <128838373+Mariam-Amin12@users.noreply.github.com>
2024-12-24 23:22:46 +0200

    FU fixed finally

--- FU.vhdl
+++ FU.vhdl
@@ -1,43 +1,41 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_ARITH.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity FU is
-    Port (
-        R1 : in STD_LOGIC_VECTOR (2 downto 0);
-        R2 : in STD_LOGIC_VECTOR (2 downto 0);
-        ex_r : in STD_LOGIC_VECTOR (2 downto 0);
-        mem_r : in STD_LOGIC_VECTOR (2 downto 0);
-        ex_reg_write : in STD_LOGIC;
-        mem_reg_write : in STD_LOGIC;
-       
-        mem_rd : in STD_LOGIC_VECTOR (2 downto 0);
-        a : out STD_LOGIC_VECTOR (1 downto 0);
-        b : out STD_LOGIC_VECTOR (1 downto 0)
+ENTITY FU IS
+    PORT (
+        R1 : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        R2 : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        ex_reg_write : IN STD_LOGIC;
+        mem_reg_write : IN STD_LOGIC;
+        ex_rd : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        mem_rd : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        a : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
+        b : OUT STD_LOGIC_VECTOR (1 DOWNTO 0)
     );
-end FU;
+END FU;
 
-architecture Behavioral of FU is
-begin
-    process(R1, R2, ex_r, mem_r, ex_reg_write, mem_reg_write, ex_rd, mem_rd)
-    begin
+ARCHITECTURE Behavioral OF FU IS
+BEGIN
+    PROCESS (R1, R2 , ex_reg_write, mem_reg_write, ex_rd, mem_rd)
+    BEGIN
         -- Forwarding for A
-        if (ex_reg_write = '1'  and ex_rd = R1) then
+        IF (ex_reg_write = '1' AND ex_rd = R1) THEN
             a <= "01"; -- Forward from EX stage
-        elsif (mem_reg_write = '1' and mem_rd = R1 and not (ex_reg_write = '1' and ex_rd = R1)) then
+        ELSIF (mem_reg_write = '1' AND mem_rd = R1 AND NOT (ex_reg_write = '1' AND ex_rd = R1)) THEN
             a <= "10"; -- Forward from MEM stage
-        else
+        ELSE
             a <= "00"; -- No forwarding
-        end if;
+        END IF;
 
         -- Forwarding for B
-        if (ex_reg_write = '1'  and ex_rd = R2) then
+        IF (ex_reg_write = '1' AND ex_rd = R2) THEN
             b <= "01"; -- Forward from EX stage
-        elsif (mem_reg_write = '1'  and mem_rd = R2 and not (ex_reg_write = '1' and ex_rd = R2)) then
+        ELSIF (mem_reg_write = '1' AND mem_rd = R2 AND NOT (ex_reg_write = '1' AND ex_rd = R2)) THEN
             b <= "10"; -- Forward from MEM stage
-        else
+        ELSE
             b <= "00"; -- No forwarding
-        end if;
-    end process;
-end Behavioral;
+        END IF;
+    END PROCESS;
+END Behavioral;
